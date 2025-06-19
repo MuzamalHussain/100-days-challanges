f2cf3e4a41e26d20048c38bf2d31e9793ab3ecc6
muzamalhussain06 <mh5071324@gmail.com>
2025-06-19 23:43:25 +0500

    day_3 uploaded

---
+++ day_3_Animated_card/day_3.js
@@ -0,0 +1,12 @@
+// Simple reveal on scroll
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+reveals.forEach((el) => observer.observe(el));
